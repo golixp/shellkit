0738ff428bd188b9c176350ef1af07335945586c
lixp <golixp@outlook.com>
2026-01-22 19:04:45 +0800

    使用正确asset语法

--- scripts/eget-download-kits.sh
+++ scripts/eget-download-kits.sh
@@ -13,41 +13,56 @@ curl -fsSL https://zyedidia.github.io/eget.sh | sh
 
 echo "Using eget to download kits to $KITS_DIR..."
 
+COMMON_ASSETS=(
+  --asset x86_64
+  --asset linux
+  --asset musl
+  --asset tar.gz
+  --asset ^deb
+  --asset ^sig
+  --asset ^sha
+)
+
+
 # Helix
-$ROOT_DIR/eget helix-editor/helix --to "$KITS_DIR" --asset "^helix-.*-x86_64.AppImage$"
+$ROOT_DIR/eget helix-editor/helix --to "$KITS_DIR" \
+--asset AppImage \
+--asset x86_64 \
+--asset linux \
+--asset ^zsync
 
 # Yazi
-$ROOT_DIR/eget sxyazi/yazi --to "$KITS_DIR" --asset "yazi-x86_64-unknown-linux-musl.zip"
+$ROOT_DIR/eget sxyazi/yazi --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # Eza
-$ROOT_DIR/eget eza-community/eza --to "$KITS_DIR" --asset "eza_x86_64-unknown-linux-musl.tar.gz"
+$ROOT_DIR/eget eza-community/eza --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # fzf
-$ROOT_DIR/eget junegunn/fzf --to "$KITS_DIR" --asset "^fzf-.*-linux_amd64.tar.gz$"
+$ROOT_DIR/eget junegunn/fzf --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # Zoxide
-$ROOT_DIR/eget ajeetdsouza/zoxide --to "$KITS_DIR" --asset "^zoxide-.*-x86_64-unknown-linux-musl.tar.gz$"
+$ROOT_DIR/eget ajeetdsouza/zoxide --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # bat
-$ROOT_DIR/eget sharkdp/bat --to "$KITS_DIR" --asset "^bat-.*-x86_64-unknown-linux-musl.tar.gz$"
+$ROOT_DIR/eget sharkdp/bat --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # fd
-$ROOT_DIR/eget sharkdp/fd --to "$KITS_DIR" --asset "^fd-.*-x86_64-unknown-linux-musl.tar.gz$"
+$ROOT_DIR/eget sharkdp/fd --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # ripgrep
-$ROOT_DIR/eget BurntSushi/ripgrep --to "$KITS_DIR" --asset "^ripgrep-.*-x86_64-unknown-linux-musl.tar.gz$"
+$ROOT_DIR/eget BurntSushi/ripgrep --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # lazygit
-$ROOT_DIR/eget jesseduffield/lazygit --to "$KITS_DIR" --asset "^lazygit_.*_linux_x86_64.tar.gz$"
+$ROOT_DIR/eget jesseduffield/lazygit --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # dust
-$ROOT_DIR/eget bootandy/dust --to "$KITS_DIR" --asset "^dust-.*-x86_64-unknown-linux-musl.tar.gz$"
+$ROOT_DIR/eget bootandy/dust --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # duf
-$ROOT_DIR/eget muesli/duf --to "$KITS_DIR" --asset "^duf-.*-linux-x86_64.tar.gz$"
+$ROOT_DIR/eget muesli/duf --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 # bottom
-$ROOT_DIR/eget ClementTsang/bottom --to "$KITS_DIR" --asset "^bottom-.*-x86_64-unknown-linux-musl.tar.gz$"
+$ROOT_DIR/eget ClementTsang/bottom --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
 
 chmod +x "$KITS_DIR"/*
 
