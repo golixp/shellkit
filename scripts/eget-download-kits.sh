81fcfd44d8f8b5112233937472ffc6bc447fd7c8
lixp <golixp@outlook.com>
2026-01-22 19:11:28 +0800

    编写正确的asset

--- scripts/eget-download-kits.sh
+++ scripts/eget-download-kits.sh
@@ -13,56 +13,77 @@ curl -fsSL https://zyedidia.github.io/eget.sh | sh
 
 echo "Using eget to download kits to $KITS_DIR..."
 
-COMMON_ASSETS=(
-  --asset x86_64
-  --asset linux
-  --asset musl
-  --asset tar.gz
-  --asset ^deb
-  --asset ^sig
-  --asset ^sha
-)
-
-
 # Helix
 $ROOT_DIR/eget helix-editor/helix --to "$KITS_DIR" \
 --asset AppImage \
 --asset x86_64 \
---asset linux \
 --asset ^zsync
 
 # Yazi
-$ROOT_DIR/eget sxyazi/yazi --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget sxyazi/yazi --to "$KITS_DIR" --asset "yazi-x86_64-unknown-linux-musl.zip"
 
 # Eza
-$ROOT_DIR/eget eza-community/eza --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget eza-community/eza --to "$KITS_DIR" --asset "eza_x86_64-unknown-linux-musl.tar.gz"
 
 # fzf
-$ROOT_DIR/eget junegunn/fzf --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget junegunn/fzf --to "$KITS_DIR" \
+--asset linux \
+--asset amd64 \
+--asset tar.gz
 
 # Zoxide
-$ROOT_DIR/eget ajeetdsouza/zoxide --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget ajeetdsouza/zoxide --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset musl \
+--asset tar.gz
 
 # bat
-$ROOT_DIR/eget sharkdp/bat --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget sharkdp/bat --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset musl \
+--asset tar.gz
 
 # fd
-$ROOT_DIR/eget sharkdp/fd --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget sharkdp/fd --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset musl \
+--asset tar.gz
 
 # ripgrep
-$ROOT_DIR/eget BurntSushi/ripgrep --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget BurntSushi/ripgrep --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset musl \
+--asset tar.gz
 
 # lazygit
-$ROOT_DIR/eget jesseduffield/lazygit --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget jesseduffield/lazygit --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset tar.gz
 
 # dust
-$ROOT_DIR/eget bootandy/dust --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget bootandy/dust --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset musl \
+--asset tar.gz
 
 # duf
-$ROOT_DIR/eget muesli/duf --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget muesli/duf --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset tar.gz
 
 # bottom
-$ROOT_DIR/eget ClementTsang/bottom --to "$KITS_DIR" "${COMMON_ASSETS[@]}"
+$ROOT_DIR/eget ClementTsang/bottom --to "$KITS_DIR" \
+--asset x86_64 \
+--asset linux \
+--asset musl \
+--asset tar.gz
 
 chmod +x "$KITS_DIR"/*
 
