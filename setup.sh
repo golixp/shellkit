c97ae8b3f500006bff2150f5553d6070cdef57f2
golixp <114489985+golixp@users.noreply.github.com>
2026-01-22 13:25:30 +0000

    完成安装脚本

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+mkdir -p $HOME/.local/share/shellkits
+
+cp -af "$SCRIPT_DIR/zsh" "$HOME/.local/share/shellkits/"
+cp -af "$SCRIPT_DIR/kits" "$HOME/.local/share/shellkits/"
+
+cp "$SCRIPT_DIR/zsh/.zshenv" "$HOME/.zshenv"
