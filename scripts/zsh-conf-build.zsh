e0515ed74d931af2aab163baca97ab193d9684fa
lixp <golixp@outlook.com>
2026-01-22 20:39:13 +0800

    优化脚本

--- scripts/zsh-conf-build.zsh
+++ scripts/zsh-conf-build.zsh
@@ -2,24 +2,28 @@
 
 set -euo pipefail
 
+# 创建相关目录
 ROOT_DIR="$(pwd)"
 ZSH_DIR="$ROOT_DIR/zsh"
-TEMP_DIR="$ROOT_DIR/temp-zsh"
 
 mkdir -p "$ZSH_DIR"
-mkdir -p "$TEMP_DIR"
 
+# 下载 Antidote
+ANTIDOTE_DIR="$ZSH_DIR/antidote"
+git clone --depth=1 https://github.com/mattmc3/antidote.git $ANTIDOTE_DIR
 
-git clone --depth=1 https://github.com/mattmc3/antidote.git $TEMP_DIR/antidote
-
-export ANTIDOTE_HOME="$ZSH_DIR/antidote"
-
-
+# 配置 Antidote
+ANTIDOTE_HOME="$ZSH_DIR/plugins"
 zstyle ':antidote:bundle' use-friendly-names 'yes'
 
-fpath=($TEMP_DIR/antidote/functions $fpath)
+# 生成插件加载文件
+fpath=($ANTIDOTE_DIR/functions $fpath)
 autoload -Uz antidote
-
 antidote bundle < $ROOT_DIR/zsh_plugins.txt > $ZSH_DIR/zsh_plugins.zsh
 
-sed -i "s|$ROOT_DIR|\$HOME/.config|g" $ZSH_DIR/zsh_plugins.zsh
+# 替换路径为配置目录
+sed -i "s|$ANTIDOTE_HOME|\$HOME/.config/zsh|g" $ZSH_DIR/zsh_plugins.zsh
+
+# 复制相关文件
+cp $ROOT_DIR/zsh_plugins.txt $ZSH_DIR/zsh_plugins.txt
+cp $ROOT_DIR/zshrc $ZSH_DIR/.zshrc
