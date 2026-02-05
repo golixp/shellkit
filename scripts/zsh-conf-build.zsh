5890fb0ec038e47d553edda96222b0f0988d611a
lixp <golixp@outlook.com>
2026-02-05 15:49:45 +0800

    zsh配置同步https://github.com/golixp/dotfiles.git

--- scripts/zsh-conf-build.zsh
+++ scripts/zsh-conf-build.zsh
@@ -8,6 +8,17 @@ ZSH_DIR="$ROOT_DIR/zsh"
 
 mkdir -p "$ZSH_DIR"
 
+# 复制相关文件
+git clone --depth=1 --recurse-submodules https://github.com/golixp/dotfiles.git $ROOT_DIR/dotfile
+ZSH_DOTFILE="$ROOT_DIR/dotfile/home/dot_config/zsh"
+
+cp -ar $ZSH_DOTFILE/conf.d $ZSH_DIR/
+cp $ZSH_DOTFILE/zsh_plugins.txt $ZSH_DIR/zsh_plugins.txt
+cp $ZSH_DOTFILE/dot_zshrc $ZSH_DIR/.zshrc
+
+cp $ROOT_DIR/p10k.zsh $ZSH_DIR/p10k.zsh
+cp $ROOT_DIR/zshenv $ZSH_DIR/.zshenv
+
 # 下载 Antidote
 ANTIDOTE_DIR="$ZSH_DIR/antidote"
 git clone --depth=1 https://github.com/mattmc3/antidote.git $ANTIDOTE_DIR
@@ -19,16 +30,10 @@ zstyle ':antidote:bundle' use-friendly-names 'yes'
 # 生成插件加载文件
 fpath=($ANTIDOTE_DIR/functions $fpath)
 autoload -Uz antidote
-antidote bundle < $ROOT_DIR/zsh_plugins.txt > $ZSH_DIR/zsh_plugins.zsh
+antidote bundle < $ZSH_DIR/zsh_plugins.txt > $ZSH_DIR/zsh_plugins.zsh
 
 # 替换路径为配置目录
 sed -i "s|\"\S*/zsh/plugins|\"\$HOME/.local/share/shellkits/zsh/plugins|g" $ZSH_DIR/zsh_plugins.zsh
-
-# 复制相关文件
-cp $ROOT_DIR/zsh_plugins.txt $ZSH_DIR/zsh_plugins.txt
-cp $ROOT_DIR/p10k.zsh $ZSH_DIR/p10k.zsh
-cp $ROOT_DIR/zshrc $ZSH_DIR/.zshrc
-cp $ROOT_DIR/zshenv $ZSH_DIR/.zshenv
 
 # 删除 zsh 插件的 .git 目录
 find $ZSH_DIR -name ".git" -type d -exec rm -rf {} +
--- zshenv
+++ zshenv
@@ -3,3 +3,13 @@ export ZDOTDIR=$HOME/.local/share/shellkits/zsh
 
 # 加入 Shellkits 工具集路径
 path+=($HOME/.local/share/shellkits/kits)
+
+# 配置默认编辑器, nvim > vim >vi
+if (( $+commands[nvim] )); then
+  export EDITOR=nvim
+elif (( $+commands[vim] )); then
+  export EDITOR=vim
+else
+  export EDITOR=vi
+fi
+export VISUAL=$EDITOR
